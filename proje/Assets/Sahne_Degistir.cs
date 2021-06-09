fa4497ffe57d0980b88a707307b5a5f381a8caaf
zubeyirakca <zbr.fb.06@gmail.com>
2021-06-09 19:57:09 +0300

    Vuforia project added.

---
+++ proje/Assets/Sahne_Degistir.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sahne_Degistir : MonoBehaviour
+{
+    public void MainToAtommodels()
+    {
+        SceneManager.LoadScene("AtomModels");
+    }
+
+    public void AtommodelsToMain()
+    {
+
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Dalton()
+    {
+        SceneManager.LoadScene("Dalton");
+    }
+
+    public void Thompson()
+    {
+        SceneManager.LoadScene("Thompson");
+    }
+
+    public void Rutherford()
+    {
+        SceneManager.LoadScene("Rutherford");
+    }
+
+    public void Bohr()
+    {
+        SceneManager.LoadScene("Bohr");
+    }
+
+    public void ToAtommodels()
+    {
+        SceneManager.LoadScene("AtomModels");
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
